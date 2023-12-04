8214747006e12400fac1d3a6c207a81a8c3a27bd
Atroras <124853947+Atroras@users.noreply.github.com>
2023-12-04 17:46:34 +0800

    adjusted gs70

--- botFunctionality/cc.js
+++ botFunctionality/cc.js
@@ -428,7 +428,7 @@ const Vhelp = (() => {
             this.#sendMessage(`${gsMsg}. ${getRandom(lowGs)}`);
           } else if (finalGearScore >= 69 && finalGearScore < 70) {
             this.#sendMessage(`${gsMsg} \n69, nice!`);
-          } else if (finalGearScore > 70 && finalGearScore < 75) {
+          } else if (finalGearScore >= 70 && finalGearScore < 75) {
             this.#sendMessage(`${gsMsg}. ${getRandom(midGs)}`);
           } else if (finalGearScore >= 75 && finalGearScore < 82) {
             this.#sendMessage(`${gsMsg}. ${getRandom(highGs)}`);
