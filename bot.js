a2dea11a8d4cab764c8727c8bc0cd8ae5da1ceda
Atroras <124853947+Atroras@users.noreply.github.com>
2024-02-28 23:50:48 +0800

    amended dc command

--- bot.js
+++ bot.js
@@ -247,7 +247,7 @@ client.on(Events.MessageCreate, async (message) => {
       //dc
       case constants.dc: {
         if (command == constants.dc) {
-          message.channel.send("https://images-ext-2.discordapp.net/external/K0vFhtTsmuL-Oto9xUyv35Ym1QcXfPNR3H0zmipH8hI/https/media.tenor.com/gsCkQq8LBngAAAPo/corvus-epic-seven.mp4")
+          message.channel.send("https://tenor.com/view/corvus-epic-seven-e7-pissed-angry-gif-13264872")
         }
         break;
       }
